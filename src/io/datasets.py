26c74b4c70c803ad59e9af8d0298e3e8b50094df
miguelmendesduarte <mdu97@hotmail.com>
2025-10-07 10:27:02 +0100

    Add module to read and write CSV data

---
+++ src/io/datasets.py
@@ -0,0 +1,43 @@
+"""Dataset readers and writers."""
+
+from pathlib import Path
+
+import pandas as pd
+from loguru import logger
+
+from .base import BaseReader, BaseWriter
+
+
+class CSVReader(BaseReader):
+    """Reader for CSV files."""
+
+    def read(self, path: Path) -> pd.DataFrame:
+        """Read data from CSV file.
+
+        Args:
+            path (Path): Path to CSV file.
+
+        Returns:
+            pd.DataFrame: Data read from CSV file.
+        """
+        logger.info(f"Reading data from {path}")
+        data = pd.read_csv(path)
+
+        if self.limit is not None:
+            data = data.head(self.limit)
+
+        return data
+
+
+class CSVWriter(BaseWriter):
+    """Writer to CSV files."""
+
+    def write(self, data: pd.DataFrame, path: Path) -> None:
+        """Write data to CSV file.
+
+        Args:
+            data (pd.DataFrame): Data to write.
+            path (Path): Path to CSV file.
+        """
+        logger.info(f"Writing data to {path}")
+        data.to_csv(path, index=False)
