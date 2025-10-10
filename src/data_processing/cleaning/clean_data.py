3451f2a0aef9013af872b57ca13935369421c0f0
miguelmendesduarte <mdu97@hotmail.com>
2025-10-10 21:19:13 +0100

    Add module to perform all cleaning steps

---
+++ src/data_processing/cleaning/clean_data.py
@@ -0,0 +1,17 @@
+"""Module for cleaning raw data."""
+
+from .players import create_players_dataset
+from .plays import create_plays_dataset
+from .tracking import clean_tracking_data_after_snap, clean_tracking_data_before_snap
+
+
+def clean_data() -> None:
+    """Clean all raw datasets and store in the cleaned data directory."""
+    create_players_dataset()
+    create_plays_dataset()
+    clean_tracking_data_before_snap()
+    clean_tracking_data_after_snap()
+
+
+if __name__ == "__main__":
+    clean_data()
